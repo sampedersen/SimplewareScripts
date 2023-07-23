e180e1402c93c937afb1cdbf2454a9a92f9843aa
sam ped <sam.mbpedersen@gmail.com>
2023-07-23 13:28:43 -0400

    Define separate_masks function, update module description, minor formatting changes

--- segmentation.py
+++ segmentation.py
@@ -4,7 +4,7 @@ segmentation.py
 Module for implementing segmentation-related functions
 
 This module contains functions for displaying a message within a dialogue box, verifies module importing to user,
-standardized tissue mask colors,
+standardized tissue mask colors, remove tissue overlap
 
 Author: Sam Pedersen
 Date: 23 July 2023
@@ -45,6 +45,7 @@ def message_box(msg):
     # Display dialogue box and message
     sip.App.GetInstance().ShowMessage(msg)
 
+
 def verify_import():
 
     """
@@ -61,6 +62,7 @@ def verify_import():
 
     sip.App.GetInstance().ShowMessage("QCFunctions module imported successfully.")
 
+
 def colors_visibility(mask):
 
     """
@@ -70,6 +72,7 @@ def colors_visibility(mask):
 
     Args:
         mask (str): Name of the mask to switch colors; must match one of the keys below
+
     Return:
         None
 
@@ -95,4 +98,66 @@ def colors_visibility(mask):
     # Set color and make visible
     sip.App.GetDocument().GetActiveGenericMask().SetColour(sip.Colour(*color))
     sip.App.GetDocument().GetActiveGenericMask().SetVisible(True)
+
+
+def separate_masks():
+
+    """
+
+    Separates/removes any overlapping/intersecting masks based on priority of tissues. Must have all 11 masks.
+
+    Args:
+        None
+
+    Return:
+        None
+
+    """
+
+    # Shorten naming structures
+    app = sip.App.GetInstance()
+    doc = app.GetDocument()
+    sliceIndices = doc.GetSliceIndices(sip.Doc.OrientationXY)
+    sliceOrientation = sip.Doc.OrientationXY
+
+    # Perform removal of overlap
+    # GM - WM
+    doc.ReplaceMaskUsingBooleanExpression("(gm MINUS wm)", doc.GetMaskByName("gm"), sliceIndices, sliceOrientation)
+    # WM - (eyes, csf)
+    doc.ReplaceMaskUsingBooleanExpression("(wm MINUS eyes MINUS csf)", doc.GetMaskByName("wm"), sliceIndices,
+                                          sliceOrientation)
+    # air - (wm, gm, csf, blood, skin)
+    doc.ReplaceMaskUsingBooleanExpression("(air MINUS wm MINUS gm MINUS csf MINUS blood MINUS skin)",
+                                          doc.GetMaskByName("air"), sliceIndices, sliceOrientation)
+    # blood - (wm, gm, eyes, skin)
+    doc.ReplaceMaskUsingBooleanExpression("(blood MINUS wm MINUS gm MINUS eyes MINUS skin)", doc.GetMaskByName("blood"),
+                                          sliceIndices, sliceOrientation)
+    # cortical - (wm, gm, air, eyes, blood)
+    doc.ReplaceMaskUsingBooleanExpression("(cortical MINUS wm MINUS gm MINUS air MINUS eyes MINUS blood)",
+                                          doc.GetMaskByName("cortical"), sliceIndices, sliceOrientation)
+    # cancellous - (wm, gm, air, eyes, blood)
+    doc.ReplaceMaskUsingBooleanExpression("(cancellous MINUS wm MINUS gm MINUS air MINUS eyes MINUS blood)",
+                                          doc.GetMaskByName("cancellous"), sliceIndices, sliceOrientation)
+    # cortical - (skin, cancellous, csf)
+    doc.ReplaceMaskUsingBooleanExpression("(cortical MINUS skin MINUS cancellous MINUS csf)", doc.GetMaskByName("gm"),
+                                          sliceIndices, sliceOrientation)
+    # cancellous - (skin, csf)
+    doc.ReplaceMaskUsingBooleanExpression("(cancellous MINUS skin MINUS csf)", doc.GetMaskByName("cancellous"),
+                                          sliceIndices, sliceOrientation)
+    # fat - (skin, wm, gm, eyes, air, blood, cortical, cancellous, csf)
+    doc.ReplaceMaskUsingBooleanExpression(
+        "(fat MINUS skin MINUS wm MINUS gm MINUS eyes MINUS air MINUS blood MINUS cortical MINUS cancellous MINUS csf)",
+        doc.GetMaskByName("fat"), sliceIndices, sliceOrientation)
+    # muscle - (skin, wm, gm, eyes, air, blood, cortical, cancellous, csf, fat)
+    doc.ReplaceMaskUsingBooleanExpression(
+        "(muscle MINUS skin MINUS wm MINUS gm MINUS eyes MINUS air MINUS blood MINUS cortical MINUS cancellous MINUS csf MINUS fat)",
+        doc.GetMaskByName("muscle"), sliceIndices, sliceOrientation)
+    # eyes - skin
+    doc.ReplaceMaskUsingBooleanExpression("(eyes MINUS skin)", doc.GetMaskByName("eyes"), sliceIndices,
+                                          sliceOrientation)
+    # csf - eyes
+    doc.ReplaceMaskUsingBooleanExpression("(csf MINUS eyes)", doc.GetMaskByName("csf"), sliceIndices, sliceOrientation)
+
+
+
 
