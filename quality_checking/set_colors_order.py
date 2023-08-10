584064dcd79135d336464cf13f2a86764aa0cde7
Sam Pedersen <70236468+sampedersen@users.noreply.github.com>
2023-08-10 15:44:11 -0400

    Update for color_palette argument

--- quality_checking/set_colors_order.py
+++ quality_checking/set_colors_order.py
@@ -29,10 +29,19 @@ Notes for use:
 - All tissue masks in the project file must have lower case names and match the spelling exactly
 - If there is a masks missing, the script will stop; if there are additional masks beyond the 11 listed above, it will
     not be effected by the script 
-- Sets the colors of the masks to pre-determined palette
-- Sets order of the tissue masks based on pre-determined priority
-- Makes all the listed masks visible  
+- Script sets the colors depending on the indicated palette choice, re-orders tissues by priority, and makes all masks 
+    visible 
+
+- Variables: 
+    - color_palette (str): Indicate whether to set tissue masks to Aprinda or Sam's color palette  
+    
 ########################################################################################################################
 """
 
-qc.colors_order_visibility()
+color_palette = "Sam"
+# color_palette = "Aprinda"                     # Example
+
+
+
+# Execute the code 
+qc.colors_order_visibility(color_palette)
