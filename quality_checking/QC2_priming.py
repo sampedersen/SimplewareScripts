a4f5d6d733160c0c9f6ee08aec7b3e352d72be42
samanthapedersen <samanthapedersen@ufl.edu>
2023-12-06 17:18:14 -0500

    Initialize user-friendly script for implementing QC2 priming steps

---
+++ quality_checking/QC2_priming.py
@@ -0,0 +1,21 @@
+"""
+QC2_priming.py
+
+Script to perform Quality Check #2 priming procedures by importing the original idv_masks and creating comparison masks
+    to display edits made during QC1 process.
+
+Author: Sam Pedersen
+Date: 2023-12-06
+
+########################################################################################################################
+
+Notes for use:
+    -
+
+Variables:
+    - Examples below
+
+
+########################################################################################################################
+"""
+
